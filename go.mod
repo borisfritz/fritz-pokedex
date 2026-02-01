5e77aaa01279a0f8cfeb6b8fa9fe0f163ef45e25
Matthew Grimes <matt.grimz@gmail.com>
2026-01-31 17:46:40 -0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/borisfritz/fritz-pokedex
+
+go 1.25.6
---
+++ repl.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"bufio"
+	"os"
+)
+
+func cleanInput(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
+}
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+		words := cleanInput(reader.Text())		
+		if len(words) == 0 {
+			continue
+		}
+		commandName := words[0]
+		fmt.Printf("Your command was: %v\n", commandName)
+	}
+}
+
