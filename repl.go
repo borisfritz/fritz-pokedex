c64abb21f27f437da1b6a9dcf91ac1a646401b3e
Matthew Grimes <matt.grimz@gmail.com>
2026-01-31 18:42:12 -0700

    updated repl structure

--- repl.go
+++ repl.go
@@ -13,13 +13,6 @@ type cliCommand struct {
 	callback func() error
 }
 
-
-func cleanInput(text string) []string {
-	output := strings.ToLower(text)
-	words := strings.Fields(output)
-	return words
-}
-
 func startRepl() {
 	reader := bufio.NewScanner(os.Stdin)
 	commands := getCommands()
@@ -45,35 +38,8 @@ func startRepl() {
 	}
 }
 
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp() error {
-	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
-	commands := getCommands()
-	if len(commands) == 0 {
-		return fmt.Errorf("Commands not found")
-	}
-	for _, cmd := range commands {
-		fmt.Printf("%v: %v\n", cmd.name, cmd.description)
-	}
-	return nil
-}
-
-func getCommands() map[string]cliCommand {
-	return map[string]cliCommand{
-		"exit": {
-			name: "exit",
-			description: "Exit the Pokedex",
-			callback: commandExit,
-		},
-		"help": {
-			name: "help",
-			description: "Displays a help message",
-			callback: commandHelp,
-		},
-	}
+func cleanInput(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
 }
---
+++ repl_commands.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		},
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
+	commands := getCommands()
+	if len(commands) == 0 {
+		return fmt.Errorf("Commands not found")
+	}
+	for _, cmd := range commands {
+		fmt.Printf("%v: %v\n", cmd.name, cmd.description)
+	}
+	return nil
+}
